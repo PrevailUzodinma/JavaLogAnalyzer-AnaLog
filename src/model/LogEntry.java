c7fb208c5324a9dcb81f89eee24270f23d1701ed
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-05 00:36:33 +0100

    changed the timestamp datatype from String to LocalDateTime

--- src/model/LogEntry.java
+++ src/model/LogEntry.java
@@ -1,20 +1,22 @@
 package model;
 
+import java.time.LocalDateTime;
+
 // this class models each line in the log
 public class LogEntry {
     // declare field variables and set them as private
-    private String timestamp;
+    private LocalDateTime timestamp;
     private String level;
     private String message;
     // implement our constructor
-    public LogEntry(String timestamp, String level, String message){
+    public LogEntry(LocalDateTime timestamp, String level, String message){
         this.timestamp = timestamp;
         this.level = level;
         this.message = message;
     }
 
     // using encapsulation, implement public methods to get each private field
-    public String getTimestamp(){
+    public LocalDateTime getTimestamp(){
         return timestamp;
     }
 
