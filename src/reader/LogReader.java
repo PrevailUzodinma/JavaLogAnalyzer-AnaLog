6304ed9125f5d1a2c526787d88a9b79baa843216
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-05 00:59:40 +0100

    set a method that will readLogs from file passed as an argumrnt using its path

--- src/reader/LogReader.java
+++ src/reader/LogReader.java
@@ -1,4 +1,10 @@
 package reader;
 
+import model.LogEntry;
+import java.io.IOException;
+import java.util.List;
+
 public interface LogReader {
+    // a method that reads logs from a file, the file path is passed as an argument
+    List<LogEntry> readLogs(String filePath) throws IOException;
 }
