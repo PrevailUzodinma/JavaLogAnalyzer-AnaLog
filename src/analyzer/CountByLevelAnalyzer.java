5ac47702eb5e8f6674c20c35233cd457af5b2b03
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-11 13:06:24 +0100

    to access the log levels, which are private variables, we use the getter method getLevel hence implementing encapsulation

--- src/analyzer/CountByLevelAnalyzer.java
+++ src/analyzer/CountByLevelAnalyzer.java
@@ -1,4 +1,20 @@
 package analyzer;
 
-public class CountByLevelAnalyzer {
+
+import model.LogEntry;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CountByLevelAnalyzer implements LogAnalyzer{
+
+    public void analyze(List<LogEntry> entries){
+        HashMap<String, Integer> levelCounts = new HashMap<>();
+
+        for (LogEntry entry : entries){
+            // ENCAPSULATION IMPLEMENTATION: using the getter method to get the value of the private variable "level"
+            String level = entry.getLevel();
+        }
+    }
 }
