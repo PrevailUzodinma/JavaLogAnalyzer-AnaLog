ede133829baf3bc3026c4a680f65ed3193de5dd7
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-11 12:17:20 +0100

    made an arraylist to dynamically store all the entries on log file as they are parsed

--- src/reader/SimpleLogReader.java
+++ src/reader/SimpleLogReader.java
@@ -12,3 +12,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Refactoring SimpleLogReader to implement LogParser instead
+
+public class SimpleLogReader {
+    private final LogParser parser;
+
+    // create a constructor, so the parser is initialized when the reader is called
+    public SimpleLogReader(LogParser parser){
+        this.parser = parser;
+    }
+
+    public List<LogEntry> readLogs(String filePath){
+        // make an arraylist to dynamically store all the entries on log file as they are parsed
+        List<LogEntry> entries = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                LogEntry entry = parser.parse(line);
+                if (entry != null){
+                    entries.add(entry);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entries;
+    }
+}
