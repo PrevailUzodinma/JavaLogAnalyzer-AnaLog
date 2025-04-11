69c37d92c98f2ff3e2c3b4c236e256c3ff0a2654
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-11 14:43:30 +0100

    Added the Keyword analyzer to count words containing a keyword

--- src/analyzer/KeywordSearchAnalyzer.java
+++ src/analyzer/KeywordSearchAnalyzer.java
@@ -1,4 +1,21 @@
 package analyzer;
 
-public class KeywordSearchAnalyzer {
+import model.LogEntry;
+import java.util.List;
+
+public class KeywordSearchAnalyzer implements LogAnalyzer{
+    private final String keyword;
+
+    public KeywordSearchAnalyzer(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public void analyze(List<LogEntry> entries) {
+        long count = entries.stream()
+                .filter(entry -> entry.getMessage().contains(keyword))
+                .count();
+
+        // Console the output
+        System.out.printf("\nNumber of logs containing the keyword '%s': %d%n", keyword, count);
+    }
 }
