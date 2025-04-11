0b4371bceadae0891b4f2eddf4e8c9b3a2b938d7
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-11 13:11:38 +0100

    Created the first implementation of LogAnalyzer to count log entries by their level (INFO, WARN, ERROR, etc).

--- src/analyzer/CountByLevelAnalyzer.java
+++ src/analyzer/CountByLevelAnalyzer.java
@@ -15,6 +15,14 @@ public class CountByLevelAnalyzer implements LogAnalyzer{
         for (LogEntry entry : entries){
             // ENCAPSULATION IMPLEMENTATION: using the getter method to get the value of the private variable "level"
             String level = entry.getLevel();
+            levelCounts.put(level, levelCounts.getOrDefault(level, 0) + 1);
+        }
+
+        // Console Output for the analyzer
+        System.out.println("Log Level Counts:");
+        // for every entry in the hashmap, print
+        for (Map.Entry<String, Integer> entry : levelCounts.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
