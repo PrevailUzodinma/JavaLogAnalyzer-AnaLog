fc49af244c9327b24da9fe75d58bce26f0531a5a
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-11 12:01:04 +0100

    mapped each line to be stored in variables timestamp, level and message

--- src/strategy/SimpleLogParser.java
+++ src/strategy/SimpleLogParser.java
@@ -12,6 +12,12 @@ public class SimpleLogParser implements LogParser {
 
     public LogEntry parse(String logLine){
         String[] parts = logLine.split(" ", 3); // timestamp, level, message
+        LocalDateTime timestamp = LocalDateTime.parse(parts[0] + " " + parts[1], formatter);
+        String[] levelAndMessage = parts[2].split(" ", 2);
+        String level = levelAndMessage[0];
 
+        String message = levelAndMessage.length > 1 ? levelAndMessage[1] : "";
+
+        return new LogEntry(timestamp, level, message);
     }
 }
