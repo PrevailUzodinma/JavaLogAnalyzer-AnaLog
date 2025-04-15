7404488c31717743550c95199251255b28c6e27c
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-15 23:53:00 +0100

    creating a decorator, a new class that implements the LogAnalyzer interface and wraps another LogAnalyzer.

---
+++ src/decorator/SummaryDecorator.java
@@ -0,0 +1,11 @@
+package decorator;
+
+import model.LogEntry;
+import strategy.LogAnalyzer;
+
+import java.util.List;
+
+// Decorator that adds a summary after the analysis
+public class SummaryDecorator implements LogAnalyzer {
+
+}
