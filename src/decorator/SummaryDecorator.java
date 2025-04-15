05c31d6e8fa918a0745c882cced3a8b8cad39acb
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-04-15 23:56:22 +0100

    starting small to test if it works

--- src/decorator/SummaryDecorator.java
+++ src/decorator/SummaryDecorator.java
@@ -7,5 +7,16 @@ import java.util.List;
 
 // Decorator that adds a summary after the analysis
 public class SummaryDecorator implements LogAnalyzer {
+    // define the analyzer to wrap
+    private final LogAnalyzer wrappedAnalyzer;
 
+    //set up the constructor, so it takes the analyzer to wrap at initialization
+    public SummaryDecorator(LogAnalyzer wrappedAnalyzer) {
+        this.wrappedAnalyzer = wrappedAnalyzer;
+    }
+    // POLYMORPHISM IN ACTION - Method overriding of analyze method from LogAnalyzer interface
+    public void analyze(List<LogEntry> logs) {
+        wrappedAnalyzer.analyze(logs); // Run the original analysis
+        System.out.println("\n📝 Summary: Processed " + logs.size() + " log entries.");
+    }
 }
